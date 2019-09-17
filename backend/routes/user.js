89cd2a3de812c907ed4f839dfdc77febbdc5e8a3
imfsdev <worldguru0922@gmail.com>
2019-09-17 23:50:20 +0200

    user authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,113 +1,111 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const validateRegisterInput = require('../validation/register');
-const validateLoginInput = require('../validation/login');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const validateRegisterInput = require("../validation/register");
+// const validateLoginInput = require('../validation/login');
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-router.post('/register', function(req, res) {
+router.post("/register", function(req, res) {
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-    const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "Email already exists"
+      });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
 
-    if(!isValid) {
-        return res.status(400).json(errors);
-    }
-    User.findOne({
-        email: req.body.email
-    }).then(user => {
-        if(user) {
-            return res.status(400).json({
-                email: 'Email already exists'
-            });
-        }
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error("There was an error", err);
         else {
-            const avatar = gravatar.url(req.body.email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            });
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                avatar
-            });
-            
-            bcrypt.genSalt(10, (err, salt) => {
-                if(err) console.error('There was an error', err);
-                else {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) console.error('There was an error', err);
-                        else {
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(user => {
-                                    res.json(user)
-                                }); 
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-router.post('/login', (req, res) => {
-
-    const { errors, isValid } = validateLoginInput(req.body);
-
-    if(!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({email})
-        .then(user => {
-            if(!user) {
-                errors.email = 'User not found'
-                return res.status(404).json(errors);
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) console.error("There was an error", err);
+            else {
+              newUser.password = hash;
+              newUser.save().then(user => {
+                res.json(user);
+              });
             }
-            bcrypt.compare(password, user.password)
-                    .then(isMatch => {
-                        if(isMatch) {
-                            const payload = {
-                                id: user.id,
-                                name: user.name,
-                                avatar: user.avatar
-                            }
-                            jwt.sign(payload, 'secret', {
-                                expiresIn: 3600
-                            }, (err, token) => {
-                                if(err) console.error('There is some error in token', err);
-                                else {
-                                    res.json({
-                                        success: true,
-                                        token: `Bearer ${token}`
-                                    });
-                                }
-                            });
-                        }
-                        else {
-                            errors.password = 'Incorrect Password';
-                            return res.status(400).json(errors);
-                        }
-                    });
-        });
+          });
+        }
+      });
+    }
+  });
 });
 
-router.get('/me', passport.authenticate('jwt', { session: false }), (req, res) => {
-    return res.json({
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email
-    });
-});
+// router.post('/login', (req, res) => {
+
+//     const { errors, isValid } = validateLoginInput(req.body);
+
+//     if(!isValid) {
+//         return res.status(400).json(errors);
+//     }
+
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     User.findOne({email})
+//         .then(user => {
+//             if(!user) {
+//                 errors.email = 'User not found'
+//                 return res.status(404).json(errors);
+//             }
+//             bcrypt.compare(password, user.password)
+//                     .then(isMatch => {
+//                         if(isMatch) {
+//                             const payload = {
+//                                 id: user.id,
+//                                 first_name: user.first_name,
+//                                 last_name: user.last_name,
+//                                 growers_id: user.growers_id,
+//                                 retail_id: user.retail_id
+//                             }
+//                             jwt.sign(payload, 'secret', {
+//                                 expiresIn: 3600
+//                             }, (err, token) => {
+//                                 if(err) console.error('There is some error in token', err);
+//                                 else {
+//                                     res.json({
+//                                         success: true,
+//                                         token: `Bearer ${token}`
+//                                     });
+//                                 }
+//                             });
+//                         }
+//                         else {
+//                             errors.password = 'Incorrect Password';
+//                             return res.status(400).json(errors);
+//                         }
+//                     });
+//         });
+// });
+
+// router.get('/me', passport.authenticate('jwt', { session: false }), (req, res) => {
+//     return res.json({
+//         id: req.user.id,
+//         name: req.user.name,
+//         email: req.user.email
+//     });
+// });
 
 module.exports = router;
--- backend/validation/register.js
+++ backend/validation/register.js
@@ -1,51 +1,71 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
-    let errors = {};
-    data.name = !isEmpty(data.name) ? data.name : '';
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
-    data.password_confirm = !isEmpty(data.password_confirm) ? data.password_confirm : '';
+  let errors = {};
 
-    if(!Validator.isLength(data.name, { min: 2, max: 30 })) {
-        errors.name = 'Name must be between 2 to 30 chars';
-    }
-    
-    if(Validator.isEmpty(data.name)) {
-        errors.name = 'Name field is required';
-    }
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+  data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+  data.growersID = !isEmpty(data.growersID) ? data.growersID : "";
+  data.retailID = !isEmpty(data.retailID) ? data.retailID : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password_confirm = !isEmpty(data.password_confirm)
+    ? data.password_confirm
+    : "";
+  console.log("data>>>", data);
+  if (!Validator.isLength(data.firstName, { min: 2, max: 30 })) {
+    errors.firstName = "Name must be between 2 to 30 chars";
+  }
 
-    if(!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-    }
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = "Name field is required";
+  }
+  if (!Validator.isLength(data.lastName, { min: 2, max: 30 })) {
+    errors.lastName = "Name must be between 2 to 30 chars";
+  }
 
-    if(Validator.isEmpty(data.email)) {
-        errors.email = 'Email is required';
-    }
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = "Name field is required";
+  }
 
-    if(!Validator.isLength(data.password, {min: 6, max: 30})) {
-        errors.password = 'Password must have 6 chars';
-    }
+  if (Validator.isEmpty(data.growersID)) {
+    errors.growersID = "Growers ID field is required";
+  }
+  if (Validator.isEmpty(data.retailID)) {
+    errors.retailID = "Retail ID field is required";
+  }
 
-    if(Validator.isEmpty(data.password)) {
-        errors.password = 'Password is required';
-    }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
 
-    if(!Validator.isLength(data.password_confirm, {min: 6, max: 30})) {
-        errors.password_confirm = 'Password must have 6 chars';
-    }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  }
 
-    if(!Validator.equals(data.password, data.password_confirm)) {
-        errors.password_confirm = 'Password and Confirm Password must match';
-    }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must have 6 chars";
+  }
 
-    if(Validator.isEmpty(data.password_confirm)) {
-        errors.password_confirm = 'Password is required';
-    }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password is required";
+  }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
+  if (!Validator.isLength(data.password_confirm, { min: 6, max: 30 })) {
+    errors.password_confirm = "Password must have 6 chars";
+  }
+
+  if (!Validator.equals(data.password, data.password_confirm)) {
+    errors.password_confirm = "Password and Confirm Password must match";
+  }
+
+  if (Validator.isEmpty(data.password_confirm)) {
+    errors.password_confirm = "Password is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
