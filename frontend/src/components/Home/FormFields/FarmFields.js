7b88c883feacb4336b4fdb62f885dde6ca8e5d76
Son, Le Cong <sonseait@gmail.com>
2019-10-21 12:47:31 +0700

    WIP: modify styles

--- frontend/src/components/Home/FormFields/FarmFields.js
+++ frontend/src/components/Home/FormFields/FarmFields.js
@@ -1,178 +1,166 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/styles'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/styles";
 
-import Button from '@material-ui/core/Button'
-import { getFarmList } from '../../../actions/farm'
-import Autocomplete from 'react-autocomplete'
-import './styles.css'
+import Button from "@material-ui/core/Button";
+import { getFarmList } from "../../../actions/farm";
+import Autocomplete from "react-autocomplete";
+import "./styles.css";
+import _ from "lodash";
 
 class FarmFields extends Component {
-  listRef = null
+  listRef = null;
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       modal: false,
-      userId:this.props.auth.user.id,
-      value:'',
-      menuProps : {
+      userId: this.props.auth.user.id,
+      value: "",
+      menuProps: {
         desktop: true,
-        disableAutoFocus: true,
+        disableAutoFocus: true
       },
-      farmlist : [],
-      selectedid : '',
-      fdata:'',
-      selecteddata:[]
+      fieldList: [],
+      allFields: [],
+      selectedid: "",
+      selecteddata: []
+    };
+  }
+
+  componentDidMount() {
+    this.props.getFarmList(this.state.userId);
+  }
+
+  handleClickAdd = () => {
+    this.props.history.push(`/farm/${this.props.auth.user.id}/fields/add`);
+  };
+
+  handleClickModify = fieldId => {
+    this.props.history.push(
+      `/farm/${this.props.auth.user.id}/fields/${fieldId}`
+    );
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.farm.getFarmsList) {
+      if (nextProps.farm.getFarmsList.length > 0) {
+        const sorted = _.orderBy(
+          nextProps.farm.getFarmsList,
+          ["fieldName"],
+          ["asc"]
+        );
+        this.setState({
+          allFields: sorted,
+          fieldList: sorted
+        });
+      } else {
+        if (this.state.allFields.length > 0) {
+          this.setState({
+            allFields: [],
+            fieldList: []
+          });
+        }
+      }
     }
   }
 
-  getData(key){
-    let selectedobj = null;
-    for (let islist = 0; islist < this.state.fdata.length; islist++)
-        {
-         if (this.state.fdata[islist]._id === key)
-            {     
-             selectedobj = this.state.fdata[islist];
-             break;
-            }          
-        }   
-    return selectedobj;
-  }
-  componentDidMount () {
-    this.props.getFarmList(this.state.userId)
-  }
-  handleClickAdd = () => {
-    this.props.history.push(`/detailfarmfield/${this.props.match.params.id}`)
-  }
-  handleClickModify = () => { 
-    this.props.history.push(`/detailfarmfield/${this.props.match.params.id}/${this.state.selectedid}`)
-  }
-
-   
-  componentWillReceiveProps(nextProps) {
-   if (nextProps.farm.getFarmsList && nextProps.farm.getFarmsList.length > 0) { 
-       this.setState({farmlist:[]});
-       this.setState({fdata:nextProps.farm.getFarmsList});
-       for (let ilist = 0; ilist < nextProps.farm.getFarmsList.length; ilist++)
-           {
-            let fr = {};
-//            fr.name =  nextProps.farm.getFarmsList[ilist].farmName+" - " +nextProps.farm.getFarmsList[ilist].clientName+ " - "+ nextProps.farm.getFarmsList[ilist].fieldName;
-            fr.name =  nextProps.farm.getFarmsList[ilist].farmName
-            fr.client = nextProps.farm.getFarmsList[ilist].clientName
-            fr.field = nextProps.farm.getFarmsList[ilist].fieldName;
-            fr.key = nextProps.farm.getFarmsList[ilist]._id;
-            this.state.farmlist.push(fr)
-           } 
-       let sdet = this.state.farmlist.sort(function(a,b){
-         return a.name - b.name
-       });
-       this.setState({farmlist:sdet});
-   }
-  } 
+  searchFields = _.debounce(keyword => {
+    if (keyword) {
+      const lowerKeyword = keyword.toLowerCase();
+      this.setState({
+        fieldList: this.state.allFields.filter(field => {
+          return (
+            field.fieldName.toLowerCase().includes(lowerKeyword) ||
+            field.farmName.toLowerCase().includes(lowerKeyword) ||
+            field.clientName.toLowerCase().includes(lowerKeyword)
+          );
+        })
+      });
+    } else {
+      this.setState({
+        fieldList: this.state.allFields
+      });
+    }
+  }, 200);
 
   render() {
-    const { classes } = this.props
+    const { classes } = this.props;
     return (
       <div>
         <div className="right-section">
-          <div id = "farmlist">
-           {this.state.farmlist.length !== 0 ? (
-            <Autocomplete
-              value={this.state.value}
-              wrapperStyle={{ position: 'relative', display: 'inline-block' }}
-              items={this.state.farmlist}
-              getItemValue={(item) => item.name}
-              shouldItemRender={(item, value) => (item.name.toLowerCase().indexOf(value.toLowerCase())) > -1}
-              onChange={(event, value) => {
-                let founditem = false; 
-                for (let ifarm = 0; ifarm < this.state.farmlist.length; ifarm++)
-                    {
-                     if (this.state.farmlist[ifarm].name.toUpperCase().indexOf(value.toUpperCase()) > -1)
-                        {
-                         founditem = true;
-                         break;
-                        }
-                    }  
-                 if (!founditem || value.length === 0)
-                    {
-                     setTimeout(()=>{
-                        document.getElementById("modify").style.visibility = "hidden";
-                     },400)
-                    }                           
-                 else
-                    {
-                     setTimeout(()=>{
-                        document.getElementById("modify").style.visibility = "visible";
-                     },400)
-                    }    
-                 this.setState({ value })
-              } }
-              onSelect={(item,value) => this.setState({"value":item,"selectedid":value.key,"selecteddata" : this.getData(value.key)})}
-              renderMenu={(items, value) => (
-                <div className="menu">
-                  {value === '' ? (
-                    <div className="item">Type of the name of a farm</div>
-                  ) : this.state.loading ? (
-                    <div className="item">Loading...</div>
-                  ) : items.length === 0 ? (
-                    <div className="item">No matches found</div>
-                  ) : items
-                  } 
-                </div>
-              )}
-              renderItem={(item, isHighlighted) => (
-                 <div style={{ background: isHighlighted ? 'lightgray' : 'lightblue', border: '1px solid',textAlign:'left'}}  key={item.key}>
-                   <div><label style = {{color:'black',opacity:.4}}>Farm   : </label>{item.name}</div>
-                   <div><label style = {{color:'black',opacity:.4}}>Client : </label>{item.client}</div>
-                   <div><label style = {{color:'black',opacity:.4}}>Field  : </label>{item.field}</div>
-                </div>
-              )}
-             />
-             ) : (
-              <div></div> 
-             ) 
-             }
-             {this.state.value.length === 0 || this.state.farmlist.length === 0 ? (
-                <Button id = 'modify' className={classes.btnStyle} style = {{visibility:'hidden',position:'absolute'}} onClick={this.handleClickModify}>
-                   + Modify the Field
-                </Button>
-              ) : (
-                <Button id = 'modify' className={classes.btnStyle} style = {{visibility:'visible',position:'absolute'}} onClick={this.handleClickModify}>
-                   + Modify the Field
-                </Button>
-               )
-              }             
-          </div>
+          <input onChange={e => this.searchFields(e.target.value)}></input>
           <div>
-             {this.state.value.length === 0 || this.state.farmlist === 0 ? (
-               <Button id = 'add' className={classes.btnStyle} style = {{visibility:'visible'}} onClick={this.handleClickAdd}>
-                  + Add a Field
-               </Button>
-             ) : (
-               <Button id = 'add' className={classes.btnStyle} style = {{visibility:'hidden'}} onClick={this.handleClickAdd}>
-                  + Add a Field
-               </Button>
-             )}
-          </div>    
+            {this.state.fieldList.map(field => {
+              return (
+                <div
+                  onClick={() => {
+                    this.handleClickModify(field._id);
+                  }}
+                  key={`field_${field._id}`}
+                  className="a_field"
+                  style={{
+                    border: "1px solid #cccccc",
+                    borderRadius: "5px",
+                    padding: "10px",
+                    marginTop: 10
+                  }}
+                >
+                  <div
+                    style={{
+                      fontSize: "15pt",
+                      fontWeight: "bold",
+                      color: "#6d6d6d"
+                    }}
+                  >
+                    {field.fieldName}
+                    <i style={{ fontSize: "11pt", fontWeight: "400" }}>
+                      ({field.approxArea} ac)
+                    </i>
+                  </div>
+                  <div style={{ fontSize: "10pt", color: "#00679a" }}>
+                    <div>
+                      <b>Farm</b>: {field.farmName}
+                    </div>
+                    <div>
+                      <b>Client</b>: {field.clientName}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <Button
+            style={{ marginTop: 10 }}
+            id="add"
+            className={classes.btnStyle}
+            onClick={this.handleClickAdd}
+          >
+            + Add a Field
+          </Button>
         </div>
         <div className="left-section"></div>
       </div>
-    )
+    );
   }
 }
 const styles = {
   btnStyle: {
-    color: '#2d7afa',
-  },
-}
+    color: "#2d7afa"
+  }
+};
+
 const mapStateToProps = state => ({
   auth: state.auth,
   errors: state.errors,
-  farm:state.farm
-})
+  farm: state.farm
+});
 
 const mapDispatchToProps = {
   getFarmList
-}
-export default connect(mapStateToProps,mapDispatchToProps)(withStyles(styles)(FarmFields))
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(FarmFields));
